b90115328178cd91c1380171b98821f45abeaa62
Finlay Smith <Finlay.Smith@nucleusfinancial.com>
2017-01-06 00:03:20 +0000

    Performance tweaking + hardcoding out of service tram detection

--- server/server.js
+++ server/server.js
@@ -1,183 +1,167 @@
 var bngconvert = require("bngconvert");
-var compress = require('compression');
-var crypto = require('crypto');
-var csv = require("csv");
-var express = require('express');
-var http = require('http');
-var moment = require('moment');
-var request = require('request');
-var winston = require('winston');
+var compression = require('compression');
+var crypto = require("crypto");
+var express = require("express");
+var http = require("http");
+var moment = require("moment");
+var replaceall = require("replaceall");
+var request = require("request");
+var timeout = require('connect-timeout');
 var zlib = require("zlib");
 
-winston.add(
-    winston.transports.File, {
-        filename: 'logs.log',
-        level: 'info',
-        json: true,
-        eol: 'n',
-        timestamp: true
-    }
-);
-
-process.on('uncaughtException', (err) => {
-    winston.log('fatal', 'Application terminated from uncaught exception.', err);
-    process.exit(1);
-});
-
 var app = express(),
-    port = (process.env.PORT || 8000),
-    maxAge = 2629746000;
+    port = (process.env.PORT || 8000);
 
-app.use(express.static('webapp', {
-    maxAge: maxAge
-}));
-app.disable('x-powered-by');
+app.use(compression());
+http.globalAgent.maxSockets = Infinity;
 
-function getAPIKey() {
-    var date = new moment().format("YYYYMMDDHH");
-    var key = crypto.createHash('md5').update("2E7S4J8WN5MK1DKPCK28YK56C" + date).digest("hex");
-    return key;
+function haltOnTimedout(req, res, next){
+    if (!req.timedout) next();
 }
 
-String.prototype.replaceAll = function (t, r) {
-    return this.split(t).join(r);
-};
+function getAPIKey() {
+    var date = moment().format("YYYYMMDDHH");
+    return crypto.createHash("md5").update("2E7S4J8WN5MK1DKPCK28YK56C" + date).digest("hex");
+}
 
-function busLocationParsing(input, service, id) {
+function busLocationParsing(input, service, id, res) {
+    res.write("[");
+
     var lines = input.split("\n"),
-        result = [],
-        headers = lines[0].split(",");
+        headers = lines[0].split(","),
+        written = 0;
 
     var minId = id;
     var maxId = id;
-    if(id) {
-        var split = id.replace(/\s+/g, '')
-                .split('-')
-                .map(str => parseInt(str));
-        if(split.length > 1) {
+    if (id) {
+        var split = id.replace(/\s+/g, "")
+            .split("-")
+            .map(str => parseInt(str));
+        if (split.length > 1) {
             minId = split[0];
             maxId = split[1];
         }
     }
-    
-    for (var i = 1; i < lines.length; i++) {
-        var vehicle = {},
-            currentline = lines[i].split(",");
-        for (var j = 0; j < headers.length; j++) {
-            if (currentline[j]) {
-                var head = headers[j].replaceAll("\"", ""),
-                    content = currentline[j].replaceAll("\"", "");
-                vehicle[head] = content;
+
+    if (lines.length > 0) {
+        for (var i = 1; i < lines.length; i++) {
+            var vehicle = {},
+                currentline = lines[i].split(",");
+            for (var j = 0; j < headers.length; j++) {
+                if (currentline[j]) {
+                    var head = replaceall("\"", "", headers[j]),
+                        content = replaceall("\"", "", currentline[j]);
+                    vehicle[head] = content;
+                }
             }
-        }
-        vehicle["busId"] = parseInt(vehicle.busId);
+            vehicle.busId = parseInt(vehicle.busId);
 
-        switch (vehicle.mnemoService === undefined ? '' : vehicle.mnemoService.substr(0, 1)) {
-        case "N":
-            vehicle.type = "night";
-            break;
-        case "T":
-            vehicle.type = "tram";
-            break;
-        default:
-            vehicle.type = "bus";
-        }
-
-        var coordinates = bngconvert.OSGB36toWGS84(vehicle.x, vehicle.y);
-        vehicle.lat = coordinates[0];
-        vehicle.lon = coordinates[1];
-
-        if(service) {
-            if (service === "All") {
-                result.push(vehicle);
-            } else if (vehicle.refService === service) {
-                result.push(vehicle);
+            switch (vehicle.mnemoService === undefined ? "" : vehicle.mnemoService.substr(0, 1)) {
+            case "N":
+                vehicle.type = "night";
+                break;
+            case "T":
+                vehicle.type = "tram";
+                break;
+            default:
+                if (vehicle.busId >= 251 && vehicle.busId <= 277) {
+                    vehicle.type = "tram";
+                } else {
+                    vehicle.type = "bus";
+                }
             }
-        }
 
-        if(id && (vehicle.busId >= minId && vehicle.busId <= maxId)) {
-            result.push(vehicle);
+            var coordinates = bngconvert.OSGB36toWGS84(vehicle.x, vehicle.y);
+            vehicle.lat = coordinates[0];
+            vehicle.lon = coordinates[1];
+
+            if (service) {
+                if (service === "All") {
+                    res.write((written > 0 ? ',' : '') + JSON.stringify(vehicle));
+                    written++;
+                } else if (vehicle.refService === service) {
+                    res.write((written > 0 ? ',' : '') + JSON.stringify(vehicle));
+                    written++;
+                }
+            } else if (id && (vehicle.busId >= minId && vehicle.busId <= maxId)) {
+                res.write((written > 0 ? ',' : '') + JSON.stringify(vehicle));
+                written++;
+            }
         }
     }
-    return JSON.stringify(result);
+    res.write(']');
+    res.end();
 }
 
 function getGzipped(url, callback) {
+    var headers = { "Accept-Encoding": "gzip" };
+
     var buffer = [];
-    http.get(url, (res) => {
-        var gunzip = zlib.createGunzip();
-        res.pipe(gunzip);
-        gunzip.on('data', (data) => {
-            buffer.push(data.toString());
-        }).on("end", () => {
-            return callback(null, buffer.join(""));
-        }).on("error", (e) => {
-            return callback(e);
-        });
-    }).on('error', (e) => {
-        return callback(e);
+    var gunzip = zlib.createGunzip();
+
+    request({ url, headers })
+        .pipe(gunzip);
+
+    gunzip.on("data", (data) => {
+        buffer.push(data.toString());
+    }).on("finish", () => {
+        return callback(undefined, buffer.join(""));
+    }).on("error", (e) => {
+        return callback(e, "");
     });
 }
 
-app.get('/getServices', (req, res) => {
+app.get("/getServices", function (req, res) {
     request({
         url: "http://ws.mybustracker.co.uk/?module=json&key=" + getAPIKey() + "&function=getServices",
         json: true
     }, (error, response, body) => {
         if (!error) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(body));
+            res.json(body);
         } else {
-            winston.log('error', 'Error fetching getServices', error);
-            res.send("Error fetching services");
+            res.status(500).end();
         }
     });
 });
 
-app.get('/getBuses/:service', (req, res) => {
+app.get("/getBuses/:service", function (req, res) {
     getGzipped("http://ws.mybustracker.co.uk/?module=csv&key=" + getAPIKey() + "&function=getVehicleLocations",
         (error, data) => {
             if (!error) {
-                res.setHeader('Content-Type', 'application/json');
-                var returnData = busLocationParsing(data, req.params.service, undefined);
-                res.send(returnData);
+                res.setHeader("Content-Type", "application/json");
+                busLocationParsing(data, req.params.service, undefined, res);
             } else {
-                winston.log('error', 'Error fetching getBuses', error);
-                res.send("Error fetching bus GPS");
+                res.status(500).end();
             }
         });
 });
 
-app.get('/getBus/:id', (req, res) => {
+app.get("/getBus/:id", function (req, res) {
     getGzipped("http://ws.mybustracker.co.uk/?module=csv&key=" + getAPIKey() + "&function=getVehicleLocations",
         (error, data) => {
             if (!error) {
-                res.setHeader('Content-Type', 'application/json');
-                var returnData = busLocationParsing(data, undefined, req.params.id);
-                res.send(returnData);
+                res.setHeader("Content-Type", "application/json");
+                busLocationParsing(data, undefined, req.params.id, res);
             } else {
-                winston.log('error', 'Error fetching getBuses', error);
-                res.send("Error fetching bus GPS");
+                res.status(500).end();
             }
         });
 });
 
-app.get('/getBusStops', (req, res) => {
+app.get("/getBusStops", function (req, res) {
     request({
         url: "http://ws.mybustracker.co.uk/?module=json&key=" + getAPIKey() + "&function=getBusStops",
         json: true
     }, (error, response, body) => {
         if (!error) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(body));
+            res.json(body);
         } else {
-            winston.log('error', 'Error fetching getBusStops', error);
-            res.send("Error fetching bus stops");
+            res.status(500).end();
         }
     });
 });
 
-app.get('/getRoute/:busId/:journeyId/:nextStop', (req, res) => {
+app.get("/getRoute/:busId/:journeyId/:nextStop", function (req, res) {
     var par = req.params;
     request({
         url: "http://ws.mybustracker.co.uk/?module=json&key=" + getAPIKey() +
@@ -187,16 +171,20 @@ app.get('/getRoute/:busId/:journeyId/:nextStop', (req, res) => {
         json: true
     }, (error, response, body) => {
         if (!error) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(body));
+            res.setHeader("Content-Type", "application/json");
+            res.json(body);
         } else {
-            winston.log('error', 'Error fetching getRoute', error);
-            res.send("Error fetching journey times");
+            res.status(500).end();
         }
     });
 });
 
-var server = app.listen(port, function () {
+app.use(express.static("webapp"));
+
+app.use(timeout(120000));
+app.use(haltOnTimedout);
+
+var server = app.listen(port, () => {
     console.log("Server running");
 });
 module.exports = server;
